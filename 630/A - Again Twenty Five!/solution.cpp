f80961ed1aae509d8ddc26679af1e1ab509d8550
Abhishek Kumar Sharma <learner.m1729@gmail.com>
2026-07-15 00:32:14 +0530

    Add Again Twenty Five! [A] from Codeforces

---
+++ 630/A - Again Twenty Five!/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl '
+'
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    long long n;
+    cin >> n;
+    
+    cout << 25 << endl;
+ 
+    return 0;
+}
