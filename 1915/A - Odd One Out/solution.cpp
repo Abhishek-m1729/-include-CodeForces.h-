993b3cfab70c86e6d69ec789b641431108ffa5ef
Abhishek Kumar Sharma <learner.m1729@gmail.com>
2026-07-15 00:29:04 +0530

    Add Odd One Out [A] from Codeforces

---
+++ 1915/A - Odd One Out/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int a, b, c;
+	    cin >> a >> b >> c;
+	    
+	    if(a == b) cout << c << "
+";
+	    else if(a == c) cout << b << "
+";
+	    else if(b == c) cout << a << "
+";
+	}
+    return 0;
+}
