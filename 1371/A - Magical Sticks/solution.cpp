fcc03b1600cc389fa866eaa1d1e9794f82db8c44
Abhishek Kumar Sharma <learner.m1729@gmail.com>
2026-07-15 00:22:48 +0530

    Add Magical Sticks [A] from Codeforces

---
+++ 1371/A - Magical Sticks/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl '
+'
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t --)
+    {
+        long long n;
+        cin >> n;
+ 
+        cout << (n + 2 - 1) / 2 << endl;
+    }
+    return 0;
+}
