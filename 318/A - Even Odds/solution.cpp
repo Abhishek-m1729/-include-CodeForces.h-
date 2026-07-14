6e46e78fe42a03b4a465f3fe2377aff12d9f2b77
Abhishek Kumar Sharma <learner.m1729@gmail.com>
2026-07-15 00:27:29 +0530

    Add Even Odds [A] from Codeforces

---
+++ 318/A - Even Odds/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	long long n, k;
+	cin >> n >> k;
+	
+    if(n%2 == 0){
+        if(n/2 >= k)
+            cout << 2*k-1 << "
+";
+        else 
+            cout << 2*(k-n/2) << "
+";
+    }
+    else{
+        if((n/2)+1 >= k)
+            cout << 2*k-1 << "
+";
+        else 
+            cout << 2*(k-n/2-1) << "
+";
+    }
+}
