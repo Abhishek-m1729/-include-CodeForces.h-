e31c645eaed39573a4027bbf92a7a75a685c8628
Abhishek Kumar Sharma <learner.m1729@gmail.com>
2026-07-15 00:35:11 +0530

    Add Minimize! [A] from Codeforces

---
+++ 2009/A - Minimize!/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '
+'
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int tc;
+    cin >> tc;
+    while(tc --)
+    {
+        int a, b;
+        cin >> a >> b;
+        
+        cout << b - a << endl;
+    }
+ 
+    return 0;
+}
