ab7594172b20842e85b71fee4d9ee27bb94bc487
Abhishek Kumar Sharma <learner.m1729@gmail.com>
2026-09-18 12:15:42 +0530

    Add Maximum GCD [A] from Codeforces

---
+++ 1370/A - Maximum GCD/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define endl '
+'
+using namespace std;
+ 
+void solution()
+{
+    int n;
+    cin >> n;
+ 
+    cout << n/2 << endl;
+}
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+        solution();
+ 
+    return 0;
+}
